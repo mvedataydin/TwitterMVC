79a04b78b1c702b71345fd5b949b19a0404c7ed3
mvedataydin <mvedataydin@gmail.com>
2020-05-28 10:12:04 +0300

    build ViewModels for index and profile pages

---
+++ TwitterMVC/ViewModels/IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitterMVC.Models;
+
+namespace TwitterMVC.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IndexViewModel()
+        {
+            UsersToFollow = new Collection<ApplicationUser>();
+        }
+        public List<Tweet> Tweets;
+        public ICollection<ApplicationUser> UsersToFollow;
+        public ApplicationUser CurrentUser;
+    }
+}
---
+++ TwitterMVC/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitterMVC.Models;
+
+namespace TwitterMVC.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ProfileViewModel()
+        {
+            UsersToFollow = new Collection<ApplicationUser>();
+        }
+        public List<Tweet> Tweets;
+        public ICollection<ApplicationUser> UsersToFollow;
+        public ApplicationUser CurrentUser;
+        public IFormFile Avatar;
+        public IFormFile Banner;
+    }
+}
